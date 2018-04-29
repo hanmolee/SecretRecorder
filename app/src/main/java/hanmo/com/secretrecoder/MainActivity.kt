ce706f1f454ed01e2755f5797ea368ad41314ee3
hanmo <dlgksah@gmail.com>
2018-04-29 19:12:16 +0900

    permission 화면 새로 구현 Overlay permission 에서 뒤로가기 시 크래시 오류 해결

--- app/src/main/java/hanmo/com/secretrecoder/MainActivity.kt
+++ app/src/main/java/hanmo/com/secretrecoder/MainActivity.kt
@@ -1,89 +1,31 @@
 package hanmo.com.secretrecoder
 
-import android.Manifest
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Build
-import android.provider.Settings
 import android.content.Intent
 import hanmo.com.secretrecoder.service.StartRecordButton
 import kotlinx.android.synthetic.main.activity_main.*
-import android.content.pm.PackageManager
-import android.Manifest.permission.RECORD_AUDIO
-import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-import android.support.v4.app.ActivityCompat
-import android.util.Log
-
 
 class MainActivity : AppCompatActivity() {
-
-    private val ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 1
-    private val RecordRequestPermissionCode = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        startActivityForResult(Intent(this@MainActivity, OverlayPermissionActivity::class.java), 2123)
+
         recoderSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
             if (isChecked) {
-                recordCheckPermissions()
-                overalyCheckPermission()
+                startService(Intent(this@MainActivity, StartRecordButton::class.java))
             } else {
                 stopService(Intent(this@MainActivity, StartRecordButton::class.java))
             }
         }
     }
 
-    fun recordCheckPermissions() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) !== PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO),
-                        RecordRequestPermissionCode)
-            } else {
-
-            }
-        }
-    }
-
-    //다시요청하는부분
-    private fun requestPermission() {
-        ActivityCompat.requestPermissions(this@MainActivity, arrayOf(WRITE_EXTERNAL_STORAGE, RECORD_AUDIO), RecordRequestPermissionCode)
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        when (requestCode) {
-            RecordRequestPermissionCode -> {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.e("Home", "Permission Granted")
-
-                } else {
-                    Log.e("Home", "Permission Failed")
-
-                }
-            }
-        }
-    }
-
-    private fun overalyCheckPermission(){
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { // 마시멜로우 이상일 경우
-            if (!Settings.canDrawOverlays(this)) {
-                val permissionActivityIntent = Intent(this  , OverlayPermissionActivity::class.java)
-                permissionActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                startActivityForResult(permissionActivityIntent, 222)
-
-                /*val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                        Uri.parse("package:$packageName"))
-                startActivityForResult(intent, ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE)*/
-            }
-        }
-    }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 222) {
-            startService(Intent(this@MainActivity, StartRecordButton::class.java))
-        }
+
     }
 
 }
--- app/src/main/java/hanmo/com/secretrecoder/OverlayPermissionActivity.kt
+++ app/src/main/java/hanmo/com/secretrecoder/OverlayPermissionActivity.kt
@@ -1,37 +1,125 @@
 package hanmo.com.secretrecoder
 
+import android.Manifest
 import android.annotation.TargetApi
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.jakewharton.rxbinding2.view.clicks
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.dialog_permission.*
 
 /**
  * Created by hanmo on 2018. 4. 26..
  */
 class OverlayPermissionActivity : AppCompatActivity() {
 
+    private val compositeDisposable = CompositeDisposable()
+    private val RecordRequestPermissionCode = 2
+    private val REQ_CODE_OVERLAY_PERMISSION = 123
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                Uri.parse("package:$packageName"))
-        startActivityForResult(intent, 123)
+        setContentView(R.layout.dialog_permission)
 
+        setButton()
     }
 
-    @TargetApi(Build.VERSION_CODES.M)
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+    private fun setButton() {
+        permissionConfirm.clicks()
+                .subscribe {
+                    recordCheckPermissions()
 
-        when (requestCode) {
-            123 -> if (Settings.canDrawOverlays(this)) {
-                finish()
+                }
+                .apply { compositeDisposable.add(this) }
+
+        permissionDenied.clicks()
+                .subscribe {
+                    setResult(Activity.RESULT_CANCELED)
+                    finish()
+                }
+                .apply { compositeDisposable.add(this) }
+    }
+
+    fun recordCheckPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) !== PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO),
+                        RecordRequestPermissionCode)
             } else {
+
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            RecordRequestPermissionCode -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.e("Home", "Permission Granted")
+                    startOverlayWindowService(this)
+                } else {
+                    Log.e("Home", "Permission Failed")
+
+                }
+            }
+        }
+    }
+
+    //다시요청하는부분
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this@OverlayPermissionActivity, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO), RecordRequestPermissionCode)
+    }
+
+    fun startOverlayWindowService(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(context)) {
+            showObtainingPermissionOverlayWindow()
+
+        } else {
+            setResult(Activity.RESULT_OK)
+            finish()
+        }
+    }
+
+    fun onOverlayResult(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (Settings.canDrawOverlays(context)) {
+                setResult(Activity.RESULT_OK)
                 finish()
             }
         }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    fun showObtainingPermissionOverlayWindow() {
+        val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
+        startActivityForResult(intent, REQ_CODE_OVERLAY_PERMISSION)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            REQ_CODE_OVERLAY_PERMISSION -> onOverlayResult(this)
+
+            else -> {
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
     }
 
 }
